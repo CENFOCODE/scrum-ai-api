821addda0078ddebe3c44590e0b49b3d44186443
EdwinM2304 <emonteroc@ucenfotec.ac.cr>
2025-11-02 21:29:17 -0600

    funcionalidad de recuperacion de contraseña con envio de contraseña temporal por correo

---
+++ src/main/java/com/project/demo/logic/entity/auth/EmailService.java
@@ -0,0 +1,22 @@
+package com.project.demo.logic.entity.auth;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailService {
+    private final JavaMailSender mailSender;
+    private final String ADMIN_GMAIL = "cenfocode@gmail.com";
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendTemporaryPassword(String toEmail, String tempPassword) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(ADMIN_GMAIL);
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject("Contraseña temporal");
+        mailMessage.setText("Esta es tu contraseña temporal: " + tempPassword);
+        mailSender.send(mailMessage);
+    }
+}
