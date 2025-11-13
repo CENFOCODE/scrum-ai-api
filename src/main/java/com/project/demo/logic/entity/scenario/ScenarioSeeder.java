5370ce81e085338e928612d311761dd66f3172af
drodriguezo18 <126374371+drodriguezo18@users.noreply.github.com>
2025-11-13 14:50:57 -0600

    cambios en las entidades y creacion de los controladores rest de simulation y simulationUser

---
+++ src/main/java/com/project/demo/logic/entity/scenario/ScenarioSeeder.java
@@ -0,0 +1,52 @@
+package com.project.demo.logic.entity.scenario;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ScenarioSeeder implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final ScenarioRepository scenarioRepository;
+
+    public ScenarioSeeder(ScenarioRepository scenarioRepository) {
+        this.scenarioRepository = scenarioRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        createCeremonies();
+    }
+
+    private void createCeremonies() {
+        if (scenarioRepository.count() > 0) return;
+
+        List<Scenario> ceremonies = List.of(
+                new Scenario(null, "Planning",
+                        "Reunión donde se define qué trabajo se realizará en el próximo sprint, se estiman tareas y se asignan responsabilidades.",
+                        120, "Backlog del proyecto actualizado", "Equipo Scrum", null,
+                        "Definir y planificar el trabajo del sprint", "Planning"),
+
+                new Scenario(null, "Daily",
+                        "Reunión diaria para sincronizar al equipo, identificar impedimentos y actualizar el progreso del trabajo.",
+                        15, "Tareas en curso", "Equipo Scrum",null,
+                        "Actualizar el progreso y resolver impedimentos", "Daily"),
+
+                new Scenario(null, "Review",
+                        "Reunión de revisión del sprint para presentar los entregables completados al Product Owner y recibir retroalimentación.",
+                        60, "Demostración de funcionalidades completadas", "Equipo Scrum y stakeholders",null,
+                        "Validar los entregables del sprint y recibir feedback","Review"),
+
+                new Scenario(null, "Retrospective",
+                        "Reunión de retrospectiva para reflexionar sobre el sprint pasado, identificar mejoras y planear acciones correctivas.",
+                        60, "Lista de mejoras y acciones de seguimiento", "Equipo Scrum",null,
+                        "Mejorar procesos y colaboración del equipo","Retrospective")
+        );
+
+
+
+        scenarioRepository.saveAll(ceremonies);
+    }
+}
---
+++ src/main/java/com/project/demo/rest/simulationUser/SimulationUserRestController.java
@@ -0,0 +1,64 @@
+package com.project.demo.rest.simulationUser;
+
+import com.project.demo.logic.entity.gift.Gift;
+import com.project.demo.logic.entity.giftList.GiftList;
+import com.project.demo.logic.entity.http.GlobalResponseHandler;
+import com.project.demo.logic.entity.http.Meta;
+import com.project.demo.logic.entity.simulation.Simulation;
+import com.project.demo.logic.entity.simulation.SimulationRepository;
+import com.project.demo.logic.entity.simulationUser.SimulationUser;
+import com.project.demo.logic.entity.simulationUser.SimulationUserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/simulationUser")
+public class SimulationUserRestController {
+    @Autowired
+    private SimulationUserRepository simulationUserRepository;
+
+    @Autowired
+    private SimulationRepository simulationRepository;
+
+    @GetMapping
+    public ResponseEntity<?> getAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            HttpServletRequest request) {
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<SimulationUser> simulationUsersPage = simulationUserRepository.findAll(pageable);
+        Meta meta = new Meta(request.getMethod(), request.getRequestURL().toString());
+        meta.setTotalPages(simulationUsersPage.getTotalPages());
+        meta.setTotalElements(simulationUsersPage.getTotalElements());
+        meta.setPageNumber(simulationUsersPage.getNumber() + 1);
+        meta.setPageSize(simulationUsersPage.getSize());
+
+        return new GlobalResponseHandler().handleResponse("Simulation user retrieved successfully",
+                simulationUsersPage.getContent(), HttpStatus.OK, meta);
+    }
+
+    @PostMapping
+    public ResponseEntity<SimulationUser> createSimulationUser(@RequestBody SimulationUser simulationUser) {
+        if (simulationUser.getSimulation() != null && simulationUser.getSimulation().getId() != null) {
+            simulationUser.setSimulation(
+                    simulationRepository.findById(simulationUser.getSimulation().getId())
+                            .orElseThrow(() -> new RuntimeException("Simulation no encontrada"))
+            );
+        } else {
+            throw new RuntimeException("Debe incluir el ID de la Simulation existente.");
+        }
+
+        SimulationUser saved = simulationUserRepository.save(simulationUser);
+        return ResponseEntity.ok(saved);
+    }
+
+
+}
