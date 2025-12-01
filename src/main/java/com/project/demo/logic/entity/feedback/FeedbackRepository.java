1f31a1ddce07561c1bc4d65bf1c50ac0cc22e0af
drodriguezo18 <126374371+drodriguezo18@users.noreply.github.com>
2025-12-01 03:16:44 -0600

    feedback entity

--- src/main/java/com/project/demo/logic/entity/feedback/FeedbackRepository.java
+++ src/main/java/com/project/demo/logic/entity/feedback/FeedbackRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-    List<Feedback> findBySimulationId(Long simulationId);
+    Optional<Feedback> findBySimulationId(Long simulationId);
 }
